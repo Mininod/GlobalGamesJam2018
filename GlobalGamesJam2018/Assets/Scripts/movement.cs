bec1ffaf82b9097ec1dd08a1108073c2b5b03222
abelitt <abelitt@hotmail.co.uk>
2018-01-26 20:54:28 +0000

    Movement And Jumping

--- GlobalGamesJam2018/Assets/Scripts/movement.cs
+++ GlobalGamesJam2018/Assets/Scripts/movement.cs
@@ -6,6 +6,10 @@ public class movement : MonoBehaviour {
 
     public float speed;
     public Rigidbody2D rb;
+    public float jumpSpeed;
+    private bool isInput;
+    
+    
 	// Use this for initialization
 	void Start ()
     {
@@ -15,25 +19,53 @@ public class movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down/*DISTANCE*/);
 
         if(Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector3(-speed, 0);
+            Movement(false);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector3(speed, 0);
+            Movement(true);
         }
 
-        else
+        if (Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = new Vector3(0, 0);
+            Jump();
+            isInput = true;
         }
+
+        if (isInput == false)
+        {
+                       
+        }
+
+        isInput = false;
         
 
 
         
 
 	}
+
+    void Movement(bool direction)
+    {
+        if (direction == false)
+        {
+            rb.velocity = new Vector3(-speed, rb.velocity.y);
+        }
+        else if (direction == true)
+        {
+            rb.velocity = new Vector3(speed, rb.velocity.y);
+        }
+
+        isInput = true;
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(transform.position.x, jumpSpeed);
+    }
 }
