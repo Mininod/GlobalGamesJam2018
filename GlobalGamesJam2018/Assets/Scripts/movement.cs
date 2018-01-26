34e10ef92d760bb1f7c7fee45c472976db2ff03f
abelitt <abelitt@hotmail.co.uk>
2018-01-26 20:13:18 +0000

    Movement Left/Right

--- GlobalGamesJam2018/Assets/Scripts/movement.cs
+++ GlobalGamesJam2018/Assets/Scripts/movement.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class movement : MonoBehaviour {
 
     public float speed;
+    public Rigidbody2D rb;
 	// Use this for initialization
 	void Start ()
     {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(speed, 0);
+        }
+
+        else
+        {
+            rb.velocity = new Vector3(0, 0);
+        }
+        
+
+
+        
+
 	}
 }
