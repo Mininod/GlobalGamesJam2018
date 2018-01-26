10e4c8b9bd7e407f1705cf260d1064c6cc357854
Olly Mills <kinglslbob@gmail.com>
2018-01-26 23:06:14 +0000

    Game Manager timer created
    something happening when past max timer needs to happen

---
+++ GlobalGamesJam2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelTimer;
+    private float currentLevelTimer;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentLevelTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentLevelTimer += Time.deltaTime;
+        print(currentLevelTimer);
+        if(currentLevelTimer>levelTimer)
+        {
+            // game over? / lose a life
+        }
+	}
+}
