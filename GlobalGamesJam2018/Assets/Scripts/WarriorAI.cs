bc16affe66c8562d92202e82fc8b65bd867bdf97
Olly Mills <kinglslbob@gmail.com>
2018-01-26 20:32:19 +0000

    warriorAiPartolFramework

--- GlobalGamesJam2018/Assets/Scripts/WarriorAI.cs
+++ GlobalGamesJam2018/Assets/Scripts/WarriorAI.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WarriorAI : MonoBehaviour {
+    public float maxMovement;
+    private float currentMovement;
+    private bool moveRight;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        moveRight = true;
+        currentMovement = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(currentMovement<maxMovement)
+        {
+            ++currentMovement;
+            // Run movement script
+        }
+        else
+        {
+            currentMovement = 0;
+            moveRight = !moveRight;
+        }
+
 	}
 }
