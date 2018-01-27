07927b614bfe25bbed5b31e55c4cd7c05be7db0c
Olly Mills <kinglslbob@gmail.com>
2018-01-27 23:15:00 +0000

    fireball

---
+++ GlobalGamesJam2018/Assets/FireBall.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+
+
+    internal Collider2D Source;
+    private float damage;
+    // Use this for initialization
+    void Start ()
+    {
+        damage = 5;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other != Source) // source is past in when cannonball is created so it can't hit itself
+        {
+            if (other.GetType() == typeof(BoxCollider2D))
+            {
+                if (other.GetComponent<MyType>())
+                {
+                    if (other.GetComponent<MyType>().mytype != MyType.objectTag.Player)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+    private float DealDamage(Collider2D other)
+    {
+        
+
+        Destroy(gameObject);
+        return damage;
+    }
+}
