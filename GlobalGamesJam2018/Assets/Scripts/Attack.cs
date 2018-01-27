01cf28e84b28701bc3b07400f64a1e1c11ba7d59
Olly Mills <kinglslbob@gmail.com>
2018-01-27 11:11:08 +0000

    is active setplayer up
    middle of coding warrior attack

--- GlobalGamesJam2018/Assets/Scripts/Attack.cs
+++ GlobalGamesJam2018/Assets/Scripts/Attack.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour {
 
+    //private GameObject trigger;
+    private BoxCollider2D trigger;
 	// Use this for initialization
 	void Start () {
-		
+		if(GetComponent<BoxCollider2D>())
+        {
+            trigger = gameObject.GetComponent<BoxCollider2D>();
+            trigger.enabled = false;
+        }
 	}
 	
 	// Update is called once per frame
@@ -22,11 +28,30 @@ public class Attack : MonoBehaviour {
 
     void SwordAttack()
     {
-
+        if(gameObject.GetComponentInParent<MyType>())
+        {
+            if (gameObject.GetComponentInParent<MyType>().mytype==MyType.objectTag.Warrior)
+            {
+                //play animation 
+                // activate hitbox
+                trigger.enabled = true;
+            }
+        }
     }
 
     void WizardAttack()
     {
 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<MyType>())
+        {
+            if(other.GetComponent<MyType>().mytype == MyType.objectTag.Player)
+            {
+
+            }
+        }
     }
 }
---
+++ GlobalGamesJam2018/Assets/Scripts/IsActivePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsActivePlayer : MonoBehaviour {
+
+    private bool ActivePlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ActivePlayer = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool getIsActivePlayer()
+    {
+        return ActivePlayer;
+    }
+    public void setActivePlayer(bool input)
+    {
+        ActivePlayer = input;
+    }
+}
