6b3e7479a4f9ee5f51d808aa75e53d651c67ce7d
abelitt <abelitt@hotmail.co.uk>
2018-01-27 11:05:41 +0000

    Transmit Detection

--- GlobalGamesJam2018/Assets/Scripts/Player.cs
+++ GlobalGamesJam2018/Assets/Scripts/Player.cs
@@ -8,19 +8,25 @@ public class Player : MonoBehaviour {
      // transfer code 
     public float hp;
     public float soulTimer;
+    public float soulTransmitDistance;
     private bool soulTimerActive;
+    public GameObject enemyLastHit;
 	// Use this for initialization
 	void Start () {
-		
+        if (soulTimer > 0)
+        {
+            soulTimerActive = true; //DELETE ME ONCE YOU NO LONGER NEED ME :,(
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {   
-        if(soulTimerActive == true)
+    {
+        if (soulTimerActive == true)
         {
             soulTimer -= Time.deltaTime;
-            if(soulTimer<0)
+            if (soulTimer < 0)
             {
                 soulTimerActive = false;
             }
@@ -37,6 +43,14 @@ public class Player : MonoBehaviour {
             if (Input.GetKey(KeyCode.Space))
             {
                 GetComponent<movement>().Jump();
+            }
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                if (enemyLastHit.transform.position.x >= transform.position.x - soulTransmitDistance && enemyLastHit.transform.position.x <= transform.position.x + soulTransmitDistance)
+                {
+                    Debug.Log("Good to transmit bb");
+                }
             }
         }
         else
