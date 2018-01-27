54d6eea896844ead6a41002e1c285556c7a5b39f
Olly Mills <kinglslbob@gmail.com>
2018-01-27 22:32:32 +0000

    goal prefab

---
+++ GlobalGamesJam2018/Assets/Scripts/winLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<IsActivePlayer>())
+        {
+            if (!other.isTrigger && other.GetComponent<IsActivePlayer>().getIsActivePlayer() == true)
+            {
+                print("win");
+
+            }
+        }
+    }
+}
