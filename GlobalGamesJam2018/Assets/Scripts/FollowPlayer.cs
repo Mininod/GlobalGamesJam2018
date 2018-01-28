ea622a76c91b40b8e768aed185f034aaf79c03ca
Olly Mills <kinglslbob@gmail.com>
2018-01-28 05:27:57 +0000

    uhh commented out follow player plan that didn't work

--- GlobalGamesJam2018/Assets/Scripts/FollowPlayer.cs
+++ GlobalGamesJam2018/Assets/Scripts/FollowPlayer.cs
@@ -19,4 +19,43 @@ public class FollowPlayer : MonoBehaviour {
     {
         player = newPlayer;
     }
+
+
+
+    //private GameObject player;
+    //private GameObject oldPos;
+
+
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    //oldPos.transform.position = new Vector3(0,0,0);
+    //    //oldPos.transform.position = new Vector3(transform.position.x, transform.position.y + 3, -10);
+    //    setOldPos(gameObject);
+    //}
+    //public void setNewPlayer(GameObject newPlayer)
+    //{
+    //    //player = newPlayer;
+
+    //    player = newPlayer;
+
+    //}
+    //public void setOldPos(GameObject Pos)
+    //{
+    //    oldPos = Pos;
+    //}
+    //// Update is called once per frame
+    //void LateUpdate()
+    //{
+    //    if (oldPos.transform.position != new Vector3(player.transform.position.x, player.transform.position.y + 3, -10))
+    //    {
+    //        Mathf.Lerp(oldPos.transform.position.x, player.transform.position.x, 5.0f);
+    //    }
+    //    else
+    //    {
+    //        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, -10);
+    //    }
+
+
+    //}
 }
