28db3240da721fd283696cea1044b51be137f71c
abelitt <abelitt@hotmail.co.uk>
2018-01-28 12:01:16 +0000

    camera

--- GlobalGamesJam2018/Assets/Scripts/FollowPlayer.cs
+++ GlobalGamesJam2018/Assets/Scripts/FollowPlayer.cs
@@ -4,58 +4,60 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
 
-    private GameObject player;
-    // Use this for initialization
-    void Start () {
+ //   private GameObject player;
+ //   // Use this for initialization
+ //   void Start () {
 		
-	}
+	//}
 	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+3, -10);
-    }
+	//// Update is called once per frame
+	//void Update () {
+ //       transform.position = new Vector3(player.transform.position.x, player.transform.position.y+3, -10);
+ //   }
 
+ //   public void setNewPlayer(GameObject newPlayer)
+ //   {
+ //       player = newPlayer;
+ //   }
+
+
+
+    private GameObject player;
+    private GameObject oldPos;
+
+
+    //Use this for initialization
+
+   void Start()
+    {
+        //oldPos.transform.position = new Vector3(0,0,0);
+        //oldPos.transform.position = new Vector3(transform.position.x, transform.position.y + 3, -10);
+       setOldPos(gameObject);
+    }
     public void setNewPlayer(GameObject newPlayer)
     {
+        //player = newPlayer;
+
         player = newPlayer;
+
     }
+    public void setOldPos(GameObject Pos)
+    {
+        oldPos = Pos;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Vector2.Distance(transform.position,player.transform.position) > 3.5f)
+        {
+            Debug.Log("lerp");
+            transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 3, -10), 0.06f);
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, -10);
+        }
 
 
-
-    //private GameObject player;
-    //private GameObject oldPos;
-
-
-    //// Use this for initialization
-    //void Start()
-    //{
-    //    //oldPos.transform.position = new Vector3(0,0,0);
-    //    //oldPos.transform.position = new Vector3(transform.position.x, transform.position.y + 3, -10);
-    //    setOldPos(gameObject);
-    //}
-    //public void setNewPlayer(GameObject newPlayer)
-    //{
-    //    //player = newPlayer;
-
-    //    player = newPlayer;
-
-    //}
-    //public void setOldPos(GameObject Pos)
-    //{
-    //    oldPos = Pos;
-    //}
-    //// Update is called once per frame
-    //void LateUpdate()
-    //{
-    //    if (oldPos.transform.position != new Vector3(player.transform.position.x, player.transform.position.y + 3, -10))
-    //    {
-    //        Mathf.Lerp(oldPos.transform.position.x, player.transform.position.x, 5.0f);
-    //    }
-    //    else
-    //    {
-    //        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, -10);
-    //    }
-
-
-    //}
+    }
 }
