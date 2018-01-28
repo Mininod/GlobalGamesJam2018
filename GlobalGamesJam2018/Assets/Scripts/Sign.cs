d5c8627716107d9044f653378a482d7bc00295ab
abelitt <abelitt@hotmail.co.uk>
2018-01-28 10:58:58 +0000

    Sign Working

---
+++ GlobalGamesJam2018/Assets/Scripts/Sign.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour {
+
+    public string textForSign;
+    private bool lookingAtSign;
+    public GameObject sign;
+    public Text UIText;
+    private bool playerInsideBox;
+	// Use this for initialization
+	void Start () {
+        lookingAtSign = false;
+        sign.GetComponentInChildren<Text>().text = textForSign;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInsideBox)
+        {
+            Debug.Log(textForSign);
+            
+            LookAtSign();
+
+        }
+        UIText.transform.position = Camera.main.WorldToScreenPoint(new Vector2(transform.position.x + 0.5f, transform.position.y + 1.5f));   	
+	} 
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject != null)
+        {
+            if (other.GetComponentInParent<IsActivePlayer>() == true)
+            {
+                if (other.GetComponentInParent<IsActivePlayer>().getIsActivePlayer() == true)
+                {
+                    playerInsideBox = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject != null)
+        {
+            if (other.GetComponentInParent<IsActivePlayer>() == true)
+            {
+                if (other.GetComponentInParent<IsActivePlayer>().getIsActivePlayer() == true)
+                {
+                    Debug.Log("SHIT");
+                    playerInsideBox = false;
+                    sign.SetActive(false);
+                }
+            }
+        }
+    }
+
+
+    void LookAtSign()
+    {
+        sign.SetActive(!sign.activeSelf);
+    }
+}
